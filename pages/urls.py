5e4b811673ff3d17d1488b508cbf3933dbb76910
The-Wall-007 <dvlpatel2410@gmail.com>
2020-10-13 20:44:49 +0530

    Created Pages and Configured navigation links

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import home
+from .views import home,car,service,contact,about
 
 
 urlpatterns = [
 
     path('', home, name='home'),
+    path('carz', car, name='car'),
+    path('service', service, name='service'),
+    path('contact', contact, name='contact'),
+    path('about', about, name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'pages/home.html')
+
+def car(request):
+    return render(request,'pages/cars.html')
+
+def service(request):
+    return render(request,'pages/services.html')
+
+def contact(request):
+    return render(request,'pages/contact.html')
+
+def about(request):
+    return render(request,'pages/about.html')
